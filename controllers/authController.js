fc5c39d87238b344728c77323bc2c3a127db0884
Aluno <rafaelbrauch994@gmail.com>
2025-09-24 17:08:49 -0300

    Aula dia 24/09

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,23 +4,25 @@ const bcrypt = require('bcryptjs');
 
 // EXEMPLO: (CREATE) POST /users/register
 exports.register = async (req, res) => {
-try {
-const { name, email, password } = req.body;
-let user = await User.findOne({ email });
-if (user) {
-return res.status(400).json({ msg: 'Um usuário com este e-mail já existe nesta dimensão.' });
-}
-user = new User({ name, email, password });
-await user.save();
-// 5. Enviar uma resposta de sucesso
-res.status(201).json({ msg: 'Usuário registrado com sucesso!' });
-} catch (err) {
-// 6. Se algo der errado, capturar o erro e enviar uma resposta de erro genérica
-res.status(500).send('Erro no servidor');
-}
-};
+    try {
+    const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+    return res.status(400).json({ msg: 'Um usuário com este e-mail já existe nesta dimensão.' });
+    }
+    user = new User({ name, email, password });
+    await user.save();
+    // 5. Enviar uma resposta de sucesso
+    res.status(201).json({ msg: 'Usuário registrado com sucesso!' });
+    } catch (err) {
+    // 6. Se algo der errado, capturar o erro e enviar uma resposta de erro genérica
+    res.status(500).send('Erro no servidor');
+    }
+    };
 
-// SUA VEZ: POST /auth/login
+
+
+// SUA VEZ: POST /auth/login    
 exports.login = async (req, res) => {
 try {
 // DICA: O processo de login é muito parecido com o de registro.
