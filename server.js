83bf3d24124aa2c044f8daa603057b3dd8839d81
Aluno <rafaelbrauch994@gmail.com>
2025-09-29 16:25:45 -0300

    Aula 29/09

--- server.js
+++ server.js
@@ -15,7 +15,19 @@ mongoose.connect(MONGO_URI)
 .catch(err => console.error('Não foi possível conectar ao MongoDB...', err));
 
 // Rotas (vamos adicionar depois)
-// app.use('/api/users', require('./routes/userRoutes'));
-// app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/users', require('./routes/productRoutes'));
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/auth', require('./controllers/authController'));
+app.use('/api/auth', require('./controllers/cartController'));
+app.use('/api/auth', require('./controllers/orderController'));
+app.use('/api/auth', require('./controllers/productController'));
+app.use('/api/auth', require('./middleware/admin'));
+app.use('/api/auth', require('./middleware/auth'));
+app.use('/api/auth', require('./middleware/validation'));
+app.use('/api/auth', require('./models/Order'));
+app.use('/api/auth', require('./models/Product'));
+app.use('/api/auth', require('./models/User'));
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na dimensão ${PORT}`));
